b9dc9e245c62494f605feb467a54732bc0bbcbdc
gitkym <singh.udayvikram95@gmail.com>
2023-03-23 07:42:16 +0000

    tests

--- scales.py
+++ scales.py
@@ -1,7 +1,12 @@
 chromatic = {'notes': [0,1,2,3,4,5,6,7,8,9,10,11], 'weights': [1]*12}
-pentatonic_minor = {'notes': [0,3,5,7,10], 'weights': [1]*5}
-pentatonic_major = {'notes': [0,2,4,7,9], 'weights': [1]*5}
+minor_pentatonic = {'notes': [0,3,5,7,10], 'weights': [1]*5}
+major_pentatonic = {'notes': [0,2,4,7,9], 'weights': [1]*5}
+major_blues = {'notes': [0,2,3,4,7,9], 'weights': [1]*6}
+minor_blues = {'notes': [0,3,5,6,7,10], 'weights': [1]*6}
 major = {'notes': [0,2,4,5,7,9,11], 'weights': [1]*7}
 minor_natural = {'notes': [0,2,3,5,7,8,10], 'weights': [1]*7}
 minor_harmonic = {'notes': [0,2,3,5,7,8,11], 'weights': [1]*7}
+whole_tone = {'notes': [0,2,4,6,8,10], 'weights': [1]*6}
+full_dim = {'notes': [0,2,3,5,6,8,10,11], 'weights': [1]*8}
+half_dim = {'notes': [0,1,3,4,6,7,9,10], 'weights': [1]*8}
 japanese = {'notes': [0,1,5,7,10], 'weights': [1]*5}
