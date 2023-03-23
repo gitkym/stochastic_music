f96e5ec3c97974693aadd40ca9b7a365ac20be79
gitkym <singh.udayvikram95@gmail.com>
2023-03-23 19:35:08 +0000

    duration_weights

--- generator.py
+++ generator.py
@@ -9,20 +9,27 @@ class Note:
         self.duration = duration
         self.velocity = velocity
 
-def generator(notes, weights=None, length=1, tpb=480, duration='fixed', velocity = 'fixed', sd=0.064, rest_probability=0.16):
-    if weights is None:
-        weights = {note: 1 for note in notes}  # set all weights to 1 if not specified
+def generator(notes, pitch_weights=None, octave_weights=None, duration_weights=None, length=1, tpb=480, 
+              duration='fixed', velocity = 'fixed', 
+              sd=0.064, rest_probability=0.16):
+    if pitch_weights is None:
+        pitch_weights = {note: 1 for note in notes}  # set all weights to 1 if not specified
+    if octave_weights is None:
+        octave_weights = {octave: 1 for octave in range(0, 9)}  # set all weights to 1 if not specified
 
     # dur = tpb
     durations = tpb*np.logspace(-5, 2, num=8, base=2) # duration of the note from 1/32 to 4
+    if duration_weights is None:
+        duration_weights = {duration: 1 for duration in durations}  # set all weights to 1 if not specified
+
     vel = 64
     sequence = []
     for _ in range(length):
         # Choose a random octave between 0 and 8
-        octave = random.choices(range(0, 9))[0]
+        octave = random.choices(range(0, 9), weights=octave_weights)[0]
         # Choose a random note from the given notes with the given weights
-        pitch = random.choices(notes, weights=weights)[0]
-        dur = random.choices(durations)[0]
+        pitch = random.choices(notes, weights=pitch_weights)[0]
+        dur = random.choices(durations, weights=duration_weights)[0]
         # Check for variables
         if duration == 'variable':
             dur = int(random.normalvariate(dur, dur * sd))
@@ -38,9 +45,11 @@ def generator(notes, weights=None, length=1, tpb=480, duration='fixed', velocity
     midi_notes = []
 
     for n in sequence:
-        note_on = mido.Message('note_on', note=n.pitch + (n.octave * 12), velocity=n.velocity, time=0)
+        note_on = mido.Message('note_on', note=n.pitch + (n.octave * 12), 
+                               velocity=n.velocity, time=0)
         midi_notes.append(note_on)
-        note_off = mido.Message('note_off', note=n.pitch + (n.octave * 12), velocity=n.velocity, time=n.duration)
+        note_off = mido.Message('note_off', note=n.pitch + (n.octave * 12), 
+                                velocity=n.velocity, time=n.duration)
         midi_notes.append(note_off)
 
     return sequence, midi_notes
